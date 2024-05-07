31930da18ca9e7a99fe20aa5fe1d5acc8f56e1cb
Martin <106612709+menkodot@users.noreply.github.com>
2024-05-07 14:11:15 +0200

    running properly after small fixes

--- frontend/stores/databaseStore.js
+++ frontend/stores/databaseStore.js
@@ -10,19 +10,19 @@ export const useDatabaseStore = defineStore("dataBaseStore", {
   }),
 
   actions: {
-
     async findAlbumCoverUrl(url) {
       console.log("findAlbumCoverUrl", url);
       const data = await $fetch("http://localhost:3001/api/albums");
-      console.log("data", data);
+      // console.log("data", data);
       const album = data.find((album) => album.albumTitle === url);
-      console.log("album", album);
-      console.log("album.albumCoverUrl", album.albumCoverUrl);
+      // console.log("album", album);
+      // console.log("album.albumCoverUrl", album.albumCoverUrl);
       this.currentCoverUrl = album.albumCoverUrl;
     },
 
     setCurrentTrackPlaying(track, file, actName, coverUrl) {
       console.log("setCurrentTrackPlaying", track, file, actName, coverUrl);
+    },
 
     async trackInfo() {
       const data = await $fetch("http://localhost:3001/api/tracksDetails");
@@ -36,7 +36,6 @@ export const useDatabaseStore = defineStore("dataBaseStore", {
       this.favorites = data;
     },
     setCurrentTrackPlaying(track, file, actName) {
-
       this.currentTrackPlaying = track;
       this.currentFilePlaying = file;
       this.currentActName = actName;
